29e13cd099f4ad636a3058c66c50dc45f23b5f42
mikezielony <mike.zielony@gmail.com>
2023-03-18 09:07:08 +0100

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  weatherApiBaseUrl: 'https://weatherapi-com.p.rapidapi.com/current.json',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'weatherapi-com.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '5bb090ca51msh520e2b5ca4fac12p1d51b6jsnf3d1409b03fd'
+};
